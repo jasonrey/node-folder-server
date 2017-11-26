7d46440534bd18504f5981fbcc043c7faf30869c
JSON object <jsonobject@outlook.com>
2017-11-26 23:14:10 +0800

    Add basic listing

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 #!/usr/bin/env node
+
+const packageData = require('./package.json')
+const path = require('path')
+const fs = require('fs')
+const program = require('commander')
+const express = require('express')
+const http = require('http')
+
+program
+  .version(packageData.version)
+  .option('-p, --port [port]', 'Define server port', 8080)
+  .option('-t, --target [path]', 'Define path to serve', '.')
+  .option('-q, --quiet', 'Disable logging', false)
+  .parse(process.argv)
+
+const target = path.resolve('.', program.target)
+
+const app = express()
+
+app.disable('X-Powered-By')
+app.set('view engine', 'pug')
+app.set('views', './src/pug')
+
+app.use((req, res, next) => {
+  const reqPath = path.resolve(target, `./${req.url}`)
+
+  let pathStat
+
+  try {
+    pathStat = fs.statSync(reqPath)
+  } catch (err) {
+    res.status(404)
+    res.write(err.message.replace(reqPath, req.url))
+    return res.end()
+  }
+
+  if (pathStat.isDirectory()) {
+    const items = fs.readdirSync(reqPath)
+
+    res.render('dir', {
+      items
+    })
+
+    return res.end()
+  }
+
+  next()
+})
+
+app.use('/', express.static(target))
+
+http
+  .createServer(app)
+  .listen(program.port, () => {
+    if (!program.quiet) {
+      console.log(`Serving ${program.target}:${program.port}`)
+    }
+  })
